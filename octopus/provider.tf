0d4f82e8e17b456739f4a2da3d75655c19fc3473
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-01 07:17:35 +1000

    no message

--- octopus/provider.tf
+++ octopus/provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
-    aws = {
+    octopusdeploy = {
       source  = "OctopusDeployLabs/octopusdeploy"
       version = "~> 0.7.68"
     }
   }
 
   backend "s3" {
-    bucket = "app-builder-58b75bdb-dc51-4f2a-a96e-ef596dcf87df"
+    bucket = "app-builder-21d7989a-b40f-4a58-95a5-df8c7d8f8109"
     key    = "appbuilder-shared-space"
     region = "us-east-1"
   }
