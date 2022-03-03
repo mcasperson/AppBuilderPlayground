8035987bb8ca45b80094764ff738ec4c15cf66bf
Matthew Casperson <matthewcasperson@gmail.com>
2022-03-03 13:49:42 +1000

    no message

--- terraform/aws-ecr/provider.tf
+++ terraform/aws-ecr/provider.tf
@@ -12,7 +12,7 @@ terraform {
 
   backend "s3" {
     bucket = "app-builder-0eeb52d7-2df7-4605-b8c7-d22afc9b018a"
-    key    = "appbuilder-shared-infrastructure"
+    key    = "appbuilder-aws-ecr"
     region = "us-east-1"
   }
 }
